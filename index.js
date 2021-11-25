afe424a15ae6f5be43c03e33f179b5673aaacaa4
ChilliD <87444703+ChilliD@users.noreply.github.com>
2021-11-24 22:54:20 -0500

    update index.js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use('/', express.static(__dirname + '/public'));
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Listening at ${PORT}`)
+});
