23b7c3f33f0dcc949f9f1ea60206bdc991df98d3
ChilliD <87444703+ChilliD@users.noreply.github.com>
2021-12-03 16:59:06 -0500

    attempt to fix mobile not playing audio

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
 app.use('/', express.static(__dirname + '/public'));
 
-server.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Listening at ${PORT}`)
 });
